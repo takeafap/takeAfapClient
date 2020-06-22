d00b148b77dbd4bbad52b30fddede6d70e8d6603
Toni Peise <toni.peise@gmx.net>
2020-06-22 16:22:46 +0200

    Navbar Styled (Placeholder Logo) + sccs vars file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,19 @@
 import { AccountService } from './_services';
 import { User } from './_models';
 
-@Component({ selector: 'app', templateUrl: 'app.component.html' })
+@Component({
+  selector: 'app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
 export class AppComponent {
-    user: User;
+  user: User;
 
-    constructor(private accountService: AccountService) {
-        this.accountService.user.subscribe(x => this.user = x);
-    }
+  constructor(private accountService: AccountService) {
+    this.accountService.user.subscribe((x) => (this.user = x));
+  }
 
-    logout() {
-        this.accountService.logout();
-    }
+  logout() {
+    this.accountService.logout();
+  }
 }
