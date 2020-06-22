0b8d5d0774497a1df4a04f6903c4f0878e5b76c4
Toni Peise <toni.peise@gmx.net>
2020-06-22 15:15:31 +0200

    Changed correctly from .less to .scss

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.less']
+  styleUrls: ['./player.component.scss'],
 })
 export class PlayerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
