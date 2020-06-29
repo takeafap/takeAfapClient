b6babe0e745d183e90ddf794bd1e848ba4d6de4a
@Noil <56548900+lionnsb@users.noreply.github.com>
2020-06-29 23:34:54 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,9 +31,6 @@ import { environment } from '../environments/environment';
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-
-    // provider used to create fake backend
-    fakeBackendProvider,
   ],
   bootstrap: [AppComponent],
 })
