4bf2b06afd3304168326eb92abea8f434f73341d
@NoilReyes <56548900+lionnsb@users.noreply.github.com>
2020-07-02 22:11:08 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'https://funhubnodeapi.herokuapp.com',
-  api2Url: 'http://localhost:3000/videos',
+  api2Url: 'https://nodejsvideocrud.herokuapp.com/tasks',
   firebase: {
     apiKey: 'AIzaSyB_9V_yUN2dOFKKVvr15f9nnVH9OKWNr18',
     authDomain: 'fun-hub-fef69.firebaseapp.com',
